e4ed90a8987a0b67d7b6327e06b78ee3d7882bf4
Rahul Arora <rahularoradfs@gmail.com>
2020-05-17 18:45:41 -0600

    reorg

--- 026.py
+++
@@ -1,3 +0,0 @@
-# numbers have finite (nonrepeating) reciprocals in base n if they share all their unique prime factors with n
-# so, only numbers of the form 2^j * 5^k will have nonrepeating representations in base 10
-# we can exclude all such numbers from our examination 
